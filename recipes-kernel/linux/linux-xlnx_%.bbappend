4c9904b7d18ca126da27291f30c2eaa9a3269654
Gabriel Lion Tran <lowe.tran@gmail.com>
2022-05-18 17:17:52 +0700

    Moved to git lfs

---
+++ recipes-kernel/linux/linux-xlnx_%.bbappend
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38c187135169f17441b7de2d006b202fde84f2a30bfaaf508b64eabde13c5585
+size 225
---
+++ ultra96v1.tcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b03fba1d24e2df8234fe8c12f031ff5bdde05d6da7f65bf463694fef579623b
+size 60566
